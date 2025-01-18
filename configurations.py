22dea19d3abbf554173f5e5317804ee15fcb773a
ShounakKulkarni <shounakkulkarni2110@gmail.com>
2025-01-18 18:06:59 +0530

    Updated code

--- configurations.py
+++ configurations.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://user1:user1pass@learn-fast-api.wvgqm.mongodb.net/?retryWrites=true&w=majority&appName=learn-fast-api"
+uri = "mongodb+srv://<username>:<password>@learn-fast-api.wvgqm.mongodb.net/?retryWrites=true&w=majority&appName=learn-fast-api"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
